d2775e607f9b5ec09a63a93e1cfc6dff9eceb5d4
codehunter <hunterx6323@gmail.com>
2022-01-16 11:42:33 +0700

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.codehunter.dontstave.cheatfx {
+    requires javafx.controls;
+//    requires org.jsoup;
+//    requires com.opencsv;
+
+    exports org.codehunter.dontstave.cheatfx;
+
+}
